14531f7604cdd9bdb18efd7ce777faeebf2a1842
Zeenat Azeem <125384808+zeenatazeem-dev@users.noreply.github.com>
2023-05-08 23:41:50 -0700

    Add files via upload
    maths methods

---
+++ maths.js
@@ -0,0 +1,85 @@
+//MATH METHODS
+  //(Math.ceil)
+  
+  //var a = Math.ceil(5.2);
+  //var a = Math.ceil(1.2)
+  //var a = Math.ceil(1.45);;
+  //var a = Math.ceil(-1.45);
+  //var a = Math.ceil(-2.35);
+  //var a = Math.ceil(2.35);
+  
+  //(Matha.floor)
+  
+  //var a = Math.floor(2.35);
+  //var a = Math.floor(2.10);
+  //var a = Math.floor(5.10);
+  //var a = Math.floor(-5.10);
+  //var a = Math.floor(-5.60);
+  //var a = Math.floor(-2.60);
+  //var a = Math.floor(0.00);
+  
+  //(Math.round)
+  
+  //var a = Math.round(0.60);
+ // var a = Math.round(2.40);
+  //var a = Math.round(2.50);
+  //var a = Math.round(2.49);
+ 
+ //(Math.trunc)
+ 
+ 
+   //var a = Math.trunc(2.49);
+   //var a = Math.trunc(8.95);
+   
+
+   //(Maths.max)
+   
+   //var a = Math.max(8, 10, 2, 50, 25);
+   
+   //(Maths.min)
+   
+   //var a = Math.min(8, 10, 2, 50, 25);
+   //var a = Math.min(-10 , -2);
+  
+   //(Math.sqrt)
+    
+   
+   //var a = Math.sqrt(4);
+   //var a = Math.sqrt(64);
+   //var a = Math.sqrt(25);
+  
+  //(Maths.cbrt)cubicroot
+  
+   //var a = Math.cbrt(125);
+   
+ //(Math.pow)
+   
+  // var a = Math.pow(4,3);//4*4*4=64
+   //var a = Math.pow(2,3);//2*2*2=8
+   //var a = Math.pow(1,1,1);
+   
+   //(Math.random)
+   
+   //var a = Math.random();
+   //var a = Math.floor(Math.random() *  10) + 1;
+   //var a = Math.floor(Math.random() * 100) + 1;
+  
+  //(Math.abs) absulute number
+  
+   //var a = Math.abs(5.25);//
+   //var a = Math.abs(-5.25);
+   //var a = Math.abs(null);
+   //var a = Math.abs("hi");
+   //var a = Math.abs(2+3);
+   
+   //(Math.PI) 22/7
+   
+   var a = Math.PI;
+
+
+
+  document.write(a);
+
+
+    
+    
